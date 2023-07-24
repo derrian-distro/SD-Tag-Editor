058713f581a3f782f98fdb52ba3f931a28886ed3
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2023-07-23 22:32:52 -0400

    further updated the tag groups,...
    Added edge cases to autoprune, to handle multiple colors, and groups larger than 1 person.
    this made the auto prune function decently bloated, but it's much more feature complete.

--- test.py
+++ test.py
@@ -3,24 +3,35 @@ from utils.interrogator import Interrogator
 import json
 import os
 import shutil
+from PIL import Image
 
 
 def main():
     inter = Interrogator()
-    folder = input("folder containing images: ")
-    image_tags = inter.interrogate_folder(folder, "moat", 0.35, 0.75, True)
+    model = inter.load_model("moat")
+    tags = inter.load_labels()
+
+    file = Image.open(input("path to image: "))
+    _, full_tags = inter.interrogate(file, model, 0.35, 0.75, tags)
+
+    # folder = input("folder containing images: ")
+    # image_tags = inter.interrogate_folder(folder, "moat", 0.35, 0.75, True)
 
     with open("test_unpruned.json", "w") as f:
-        json.dump(image_tags, f, indent=2)
+        json.dump(full_tags, f, indent=2)
 
-    # with open("test_unpruned.json", "r") as f:
-    #     image_tags = json.load(f)
-
-    for value in image_tags.values():
-        value = (value[0], value[1], prune_tags(value[2]))
-
+    full_tags = prune_tags(full_tags)
     with open("test.json", "w") as f:
-        json.dump(image_tags, f, indent=2)
+        json.dump(full_tags, f, indent=2)
+
+    # # with open("test_unpruned.json", "r") as f:
+    # #     image_tags = json.load(f)
+
+    # for value in full_tags.values():
+    #     value = (value[0], value[1], prune_tags(value[2]))
+
+    # with open("test.json", "w") as f:
+    #     json.dump(image_tags, f, indent=2)
 
 
 if __name__ == "__main__":
--- utils/autoprune.py
+++ utils/autoprune.py
@@ -1,14 +1,83 @@
-def prune_tags(tag_group: dict, base_tag: str = None) -> dict:
-    for key in tag_group:
-        if isinstance(tag_group[key], dict):
-            prune_tags(tag_group[key], base_tag if key == "base" else key)
-        elif len(tag_group[key]) > 1:
-            if key in ["other", "modifier", "character", "nudity"]:
-                continue
-            sorted_array = sorted(
-                tag_group[key], key=lambda x: list(x.values())[0], reverse=True
+from typing import Any, Dict
+
+
+SKIP_KEYS = ["other", "modifier", "character", "nudity"]
+MODIFIER_TAGS = {
+    "multicolored": 1,
+    "heterochromia": 2,
+    "two-tone": 2,
+    "gradient": 2,
+}
+GROUP_PRUNE_LIST = ["solo", "1girl", "1boy", "1other"]
+
+
+def prune_tags(tag_group: Dict[str, Any]) -> Dict[str, Any]:
+    tag_group["image composition"]["group"] = [
+        sorted(tag_group["image composition"]["group"], key=sorted_func, reverse=True)[
+            0
+        ]
+    ]
+    dont_prune = tag_group["image composition"]["group"] not in GROUP_PRUNE_LIST
+    return prune_tags_helper(tag_group, dont_prune=dont_prune)
+
+
+def prune_tags_helper(
+    tag_group: Dict[str, Any],
+    base_tag: str = None,
+    dont_prune: bool = None,
+    top_level: str = None,
+) -> Dict[str, Any]:
+    for key, group in tag_group.items():
+        if isinstance(group, dict):
+            prune_tags_helper(
+                group,
+                base_tag if key == "base" else key,
+                dont_prune,
+                top_level or key,
             )
-            if base_tag in sorted_array[0] or key in sorted_array[0]:
-                sorted_array.pop(0)
-            tag_group[key] = [sorted_array[0]]
+        elif len(group) > 1 and key not in SKIP_KEYS:
+            keep_array = []
+            sorted_array = sorted(group, key=sorted_func, reverse=True)
+            if top_level in {"attire", "body trait"} and dont_prune:
+                handle_base_tag(sorted_array, base_tag, key)
+                tag_group[key] = sorted_array
+                continue
+            modifier = handle_priority(sorted_array, keep_array)
+            for tag in sorted_array:
+                if base_tag in tag or key in tag:
+                    continue
+                keep_array.append(tag)
+                modifier -= 1
+                if modifier < 1:
+                    break
+            tag_group[key] = keep_array
     return tag_group
+
+
+def handle_base_tag(sorted_array: list, base_tag: str, current_key: str) -> list:
+    if base_tag in sorted_array[0] or current_key in sorted_array[0]:
+        sorted_array.pop(0)
+    return sorted_array
+
+
+def handle_priority(sorted_array: list, keep_array: list) -> int:
+    priority = 0
+    modifier = 0
+    while "priority" in sorted_array[0]:
+        if sorted_array[0]["priority"] <= priority:
+            sorted_array.pop(0)
+            continue
+        modifier = 2
+        priority = sorted_array[0]["priority"]
+        if not keep_array:
+            keep_array.append(sorted_array[0])
+        keep_array[0] = sorted_array[0]
+        sorted_array.pop(0)
+    return modifier
+
+
+def sorted_func(x: dict) -> float:
+    tag = list(x.keys())[0].split(" ")[0]
+    if tag in MODIFIER_TAGS:
+        x["priority"] = MODIFIER_TAGS[tag]
+    return x.get("priority", list(x.values())[0])
