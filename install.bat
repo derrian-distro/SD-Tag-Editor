8619241f6c5277ccbe702ddd2d321bce26db4a93
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2023-11-19 11:04:51 -0500

    re-add import to torch so that onnxruntime actually can use it

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 python -m venv venv
 call .\venv\Scripts\activate
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
 pause
