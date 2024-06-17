8d145b6685d70dc389e465add1a4dd35dc132e03
Miyuutsu <Miyuu@miyuu.pw>
2024-06-17 10:56:12 -0400

    Update install.sh
    The previous "fix" is no longer needed and is in fact a detriment.

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@
 python3.10 -m venv venv
 source venv/bin/activate
 python -m pip install -r requirements.txt
-python -m pip install onnxruntime # necessary due to cuda incompatibility with onnxruntime-gpu with cuda 12.1 on Linux, causes speed loss but alternative is not working at all
 deactivate
