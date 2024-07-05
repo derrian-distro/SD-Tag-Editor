0596674271fa7b1bc6b4f96518853ad4b6c0454a
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2024-07-05 15:10:07 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ if [ -d "venv"]; then
 else
     ./install.sh no_pause
     source venv/bin/activate
+fi
 python run.py --batch_size=1 --gen_threshold=0.35 --char_threshold=0.75
 read -p "Press any key to continue..."
 deactivate
