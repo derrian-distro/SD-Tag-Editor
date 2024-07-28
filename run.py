397e71ba77ed509e5eac89bf00840a8def6af474
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2024-07-28 10:11:39 -0400

    added vit-large to the repo list

--- run.py
+++ run.py
@@ -21,6 +21,7 @@ import tqdm
 torch_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MODEL_REPO_MAP = {
     "vit": "SmilingWolf/wd-vit-tagger-v3",
+    "vit-large": "SmilingWolf/wd-vit-large-tagger-v3",
     "swinv2": "SmilingWolf/wd-swinv2-tagger-v3",
     "convnext": "SmilingWolf/wd-convnext-tagger-v3",
 }
